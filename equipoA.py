3ef82b3dec6627aa63350c1c8b0a488ffe359007
Andresleiva48 <1053867562@teinco.edu.co>
2021-09-01 09:38:17 -0500

    Creacion Variables
    Se crean las entradas de los archivos

--- equipoA.py
+++ equipoA.py
@@ -1,1 +1,7 @@
-#Software
+#Software  || Online: 6 puntos, Presencial: 10 puntos, Otros: 1 punto
+
+InfoCompra= input("Digite que va comprar: ")
+CantCompra= int(input("Digite la cantidad a comprar: "))
+MedCompra= int(input("Por que medio compra?: "))
+
+
