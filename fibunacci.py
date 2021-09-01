6cf3b5bd92a02b3fa40650898666c0026027462c
andresC-sup <camilosama6@gmail.com>
2021-09-01 08:59:49 -0500

    cambio print
    empanada

--- fibunacci.py
+++ fibunacci.py
@@ -3,9 +3,17 @@ numero = 0
 numero2 = int(input("Digite un numero"))
 for x in range(numero2):
     print(f"{x}")
+
     print(x)
 
 print("hola1")
 print("hola2")
 print("hola3")
+
+
+    
+print("SSSSSSSSSSSsssss")
+print("SSSSSSSSSSSsssss")
+print("SSSSSSSSSSSsssss")
+print("SSSSSSSSSSSsssss")
 
