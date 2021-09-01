e6ef86253da78f85d14dbf9734e54d1978d397fa
SalcedoFelipeTeinco <89918714+SalcedoFelipeTeinco@users.noreply.github.com>
2021-09-01 09:31:16 -0500

    Prroooos Parametros
    Dividamonos el trabajo en esos parametros

---
+++ EquipoB.py
@@ -0,0 +1,6 @@
+#Dividamos el trabajo en 4
+
+#Funcion del hotel que es un array de 10 espacios
+#For con el requerimiento de datos
+#For con la paginacion de las personas en los cuartos
+#Ejecucion y IF
