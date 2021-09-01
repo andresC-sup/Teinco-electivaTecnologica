b2e3f3f065706371a584aad1d861ae299779773d
Brandon192002 <89918759+Brandon192002@users.noreply.github.com>
2021-09-01 08:53:46 -0500

    Fusion codigo fabian

--- fibunacci.py
+++ fibunacci.py
@@ -1,7 +1,11 @@
 # Aca se realizda un codigo fibunacci
-
 numero = 0
 numero2 = int(input("Digite un numero"))
 for x in range(numero2):
     print(f"{x}")
     print(x)
+
+print("hola1")
+print("hola2")
+print("hola3")
+
